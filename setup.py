0284f3a9eed4b343fb82c8579df53ce2ca18628c
Rico Vanderhallen <65690008+ricovdh@users.noreply.github.com>
2024-06-03 19:10:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ct-configcreator",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[],
     author="Clevrthings",
